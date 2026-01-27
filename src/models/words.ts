f7932a45fbc1203ad35e8c3ebdaf1e1493417486
little_q_exist <3061982805@qq.com>
2026-01-27 10:54:24 +0800

    refactor: 完善了schema的验证

--- src/models/words.ts
+++ src/models/words.ts
@@ -1,14 +1,38 @@
 import mongoose from 'mongoose';
 
+const definitionSchema = new mongoose.Schema({
+  meaning: { type: String, required: [true, 'Must have a meaning'] },
+  partOfSpeech: {
+    type: String,
+    required: true,
+    enum: ['verb', 'noun', 'adjective', 'adverb', 'preposition'],
+  },
+});
+
+const userLearningSchema = new mongoose.Schema({
+  userId: mongoose.Types.ObjectId,
+  familiarity: { type: Number, required: true, default: 0, enum: [0, 1, 2, 3] },
+  favorited: { type: Boolean, required: true, default: false },
+  mastered: { type: Boolean, required: true, default: false },
+});
+
 const wordSchema = new mongoose.Schema({
-  definitions: [{ meaning: String, partOfSpeech: String }],
-  english: String,
-  exampleSentence: String,
-  familiarity: Number,
-  favorited: Boolean,
-  mastered: Boolean,
-  phonetic: String,
+  definitions: {
+    type: [definitionSchema],
+    required: true,
+    validate: {
+      validator: function (v: Array<unknown>) {
+        return v.length > 0;
+      },
+      message: 'Must have at least 1 definition',
+    },
+  },
+  english: { type: String, required: true, lowercase: true, index: true, trim: true },
+  exampleSentence: { type: String, required: true },
+  phonetic: { type: String, required: true },
   related: String,
+  userLearningData: [userLearningSchema],
+  createdAt: { type: Date },
 });
 
 export default mongoose.model('Word', wordSchema);
