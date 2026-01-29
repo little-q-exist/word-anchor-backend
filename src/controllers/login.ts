f3cb319c322839b50565963216b7eac4c7cfc02a
little_q_exist <3061982805@qq.com>
2026-01-29 19:55:23 +0800

    feat: 增加了登录路由

---
+++ src/controllers/login.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+import User from '../models/users.js';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+
+  const userInDB = await User.findOne({ username: username });
+
+  if (!userInDB) {
+    return res.status(401).json({ error: 'invalid username' });
+  }
+
+  const passwordCorrect = bcrypt.compare(userInDB.passwordHash, password);
+
+  if (!passwordCorrect) {
+    return res.status(401).json({ error: 'invalid password' });
+  }
+
+  const token = jwt.sign({ username, _id: userInDB._id }, process.env.SECRET || 'SECRET');
+
+  res.json({ token, username, _id: userInDB._id });
+});
+
+export default router;
