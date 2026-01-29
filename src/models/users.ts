0e919651857772733799f29850b114c2ff4e8757
little_q_exist <3061982805@qq.com>
2026-01-29 10:27:30 +0800

    refactor: 把单词学习数据迁移到user文档中

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,8 +1,39 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+interface UserLearningData {
+  familiarity: number;
+  favorited: boolean;
+  mastered: boolean;
+  wordId?: mongoose.Types.ObjectId | null;
+}
+
+const userLearningSchema = new mongoose.Schema<UserLearningData>({
+  wordId: { type: mongoose.Types.ObjectId, ref: 'Word' },
+  familiarity: { type: Number, required: true, default: 0, enum: [0, 1, 2, 3] },
+  favorited: { type: Boolean, required: true, default: false },
+  mastered: { type: Boolean, required: true, default: false },
+  // TODO: 用户复习进度
+});
+
+interface User {
+  username: string;
+  passwordHash: string;
+  userLearningData: UserLearningData[];
+}
+
+type THydratedUserDocument = {
+  username: string;
+  passwordHash: string;
+  userLearningData?: mongoose.Types.DocumentArray<UserLearningData>;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+type UserModelType = mongoose.Model<User, {}, {}, {}, THydratedUserDocument>;
+
+const userSchema = new mongoose.Schema<User, UserModelType>({
   username: { type: String, required: true },
   passwordHash: { type: String, required: true },
+  userLearningData: [userLearningSchema],
 });
 
 export default mongoose.model('User', userSchema);
