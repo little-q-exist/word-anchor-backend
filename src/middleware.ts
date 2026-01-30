96217fdf1958a7a33ca23a9fa664394aea3beabe
little_q_exist <3061982805@qq.com>
2026-01-30 10:36:35 +0800

    feat: 增加了提取和验证token的中间件

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,43 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt, { JsonWebTokenError } from 'jsonwebtoken';
 
-export const tokenExtractor = (req: Request, _res: Response, next: NextFunction) => {
+import User from './models/users.js';
+
+const tokenExtractor = (req: Request, res: Response, next: NextFunction) => {
   const tokenHeader = 'Bearer ';
   const authorization = req.get('authorization');
-  let token;
   if (authorization && authorization.startsWith(tokenHeader)) {
-    token = authorization.replace(tokenHeader, '');
+    res.locals.token = authorization.replace(tokenHeader, '');
   }
-  req.body = { ...req.body, token };
   next();
 };
+
+const tokenAuthenticator = async (req: Request, res: Response, next: NextFunction) => {
+  const token = res.locals.token;
+  if (!token) {
+    res.status(401).json({ error: 'invalid token' });
+  }
+  try {
+    const decodedToken = JSON.parse(String(jwt.verify(token, process.env.SECRET || 'SECRET')));
+    const userInDB = await User.findById(decodedToken._id);
+    if (!userInDB) {
+      return res.status(401).json({ error: 'invalid user' });
+    }
+    res.locals.user = userInDB;
+    next();
+  } catch (error) {
+    if (error instanceof JsonWebTokenError) {
+      res.status(401).json({ error: { name: error.name, message: error.message } });
+    } else {
+      res.status(401).json({ error: error });
+      console.info(error);
+    }
+  }
+};
+
+/**
+ * extract token in request.authorization, verify and find the corresponding user.
+ * Access the token through res.locals.token.
+ * Access the user through res.locals.user.
+ */
+export const authTokenMiddleware = [tokenExtractor, tokenAuthenticator];
