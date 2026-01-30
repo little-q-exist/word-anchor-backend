5988ea421f30bf0ee8c679cdb187ae64d76756bf
little_q_exist <3061982805@qq.com>
2026-01-30 11:53:03 +0800

    feat: 单词patch路由现在可以正常修改熟悉度了

--- src/controllers/word.ts
+++ src/controllers/word.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 
 import Word, { type Word as WordType } from '../models/words.js';
+import { type UserLearningData } from '../models/users.js';
+import { authTokenMiddleware } from '../middleware.js';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
@@ -11,6 +14,7 @@ router.get('/', async (_req, res) => {
 
 router.put(
   '/:id',
+  authTokenMiddleware,
   async (req: Request<{ id: string }, unknown, WordType>, res: Response<WordType | null>) => {
     const updatedWord = req.body;
     const word = await Word.findByIdAndUpdate(req.params.id, updatedWord, {
@@ -22,17 +26,56 @@ router.put(
 
 router.patch(
   '/:id/familiarity',
+  authTokenMiddleware,
   async (
     req: Request<{ id: string }, unknown, { familiarity: number }>,
-    res: Response<WordType>
+    res: Response<UserLearningData | { error: string }>
   ) => {
-    const { familiarity } = req.body;
-    const word = await Word.findById(req.params.id);
-    if (word) {
-      // TODO: change familiarity
-      return res.json(await word?.save());
-    } else {
-      return res.status(404).end();
+    try {
+      const user = res.locals.user;
+      const familiarity = req.body.familiarity;
+      const wordId = req.params.id;
+
+      if (![0, 1, 2, 3].includes(familiarity)) {
+        return res.json({ error: 'invalid familiarity' });
+      }
+
+      if (!wordId || !mongoose.Types.ObjectId.isValid(wordId)) {
+        return res.json({ error: 'invalid word Id' });
+      }
+
+      if (!user) {
+        return res.json({ error: 'user not authenticated' });
+      }
+
+      user.userLearningData = user.userLearningData || [];
+
+      let wordDoc = user.userLearningData.find(
+        (data: UserLearningData) => data.wordId?.toString() === wordId
+      );
+
+      if (!wordDoc) {
+        wordDoc = {
+          familiarity,
+          favorited: false,
+          mastered: false,
+          wordId: new mongoose.Types.ObjectId(wordId),
+        };
+        user.userLearningData.push(wordDoc);
+      } else {
+        wordDoc.familiarity = familiarity;
+      }
+
+      wordDoc.mastered = familiarity === 3;
+
+      const updatedUser = await user.save();
+      const newWordDoc = updatedUser.userLearningData.find(
+        (data: UserLearningData) => data.wordId.toString() === wordId
+      );
+      res.json(newWordDoc);
+    } catch (error) {
+      console.info(error);
+      return res.status(500).json({ error: 'internal server error' });
     }
   }
 );
